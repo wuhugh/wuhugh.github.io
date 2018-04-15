360c6dfcc54bb9abcea9b6e5838f439e72372319
Hugh <hugh.j.wu@gmail.com>
2018-04-14 20:45:48 -0400

    Updated main page layout

--- js/effects.js
+++ js/effects.js
@@ -1,27 +1,3 @@
-// TODO: 
-// Refactor
-// Add animations
-
-/*
-$("#left-pane").click(function(){
-    $("#left-pane").css({
-        "display": "none"
-    });
-    $("#collapsed-pane").css({
-        "display": "block"
-    });
-    $("#main-content").removeClass("pure-u-md-20-24");
-    $("#main-content").addClass("pure-u-sm-23-24");
+document.addEventListener("DOMContentLoaded", function() {
+  // document.getElementById("about-me").hidden = true;
 });
-
-$("#collapsed-pane").click(function(){
-    $("#collapsed-pane").css({
-        "display": "none"
-    });
-    $("#left-pane").css({
-        "display": "block"
-    });
-    $("#main-content").removeClass("pure-u-sm-23-24");
-    $("#main-content").addClass("pure-u-md-20-24");
-});
-*/

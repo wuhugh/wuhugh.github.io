4cfaff0f5201908a30bf81106f8138d9c9cf55a9
Hugh <hugh.j.wu@gmail.com>
2018-05-12 00:26:29 -0400

    Updated way the sections of the page are loaded

--- js/effects.js
+++ js/effects.js
@@ -1,3 +1,77 @@
+// Once the document has loaded, fetch all the possible views and attach listeners to display the
+// proper view onclick
 document.addEventListener("DOMContentLoaded", function() {
-  // document.getElementById("about-me").hidden = true;
+  fetch("/views/about-me.html")
+    .then(response => {
+      return response.text();
+    })
+    .then(htmlData => {
+      let aboutMe = document.getElementById("about-me");
+      aboutMe.innerHTML = htmlData;
+      aboutMe.style.display = "block";
+    });
+
+  fetch("/views/experience.html")
+    .then(response => {
+      return response.text();
+    })
+    .then(htmlData => {
+      let experience = document.getElementById("experience");
+      experience.innerHTML = htmlData;
+      experience.style.display = "none";
+    });
+
+  fetch("/views/projects.html")
+    .then(response => {
+      return response.text();
+    })
+    .then(htmlData => {
+      let projects = document.getElementById("projects");
+      projects.innerHTML = htmlData;
+      projects.style.display = "none";
+    });
+
+  fetch("/views/links.html")
+    .then(response => {
+      return response.text();
+    })
+    .then(htmlData => {
+      let links = document.getElementById("links");
+      links.innerHTML = htmlData;
+      links.style.display = "none";
+    });
+
+  setupTabListeners();
 });
+
+function setupTabListeners() {
+  document.getElementById("about-me-tab").addEventListener("click", function() {
+    toggleVisibility("about-me");
+  });
+
+  document.getElementById("experience-tab").addEventListener("click", function() {
+    toggleVisibility("experience");
+  });
+
+  document.getElementById("projects-tab").addEventListener("click", function() {
+    toggleVisibility("projects");
+  });
+
+  document.getElementById("links-tab").addEventListener("click", function() {
+    toggleVisibility("links");
+  });
+}
+
+// Hides all views except the one whose div ID is passed in
+function toggleVisibility(divId) {
+  let views = Array.from(document.getElementById("current-display").children);
+
+  views.forEach(item => {
+    if (item.id === divId) {
+      item.style.display = "block";
+    }
+    else {
+      item.style.display = "none";
+    }
+  })
+}
